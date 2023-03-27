2c94a4c92efdcf07310da02f39c0f3ebc13f4da4
Erwin Medina <erwin.l.medina@gmail.com>
2023-03-27 14:54:22 -0700

    finalized main.cpp, output file works

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Assignment 1 - Lexer/Scanner
 
 ## Objective:
-To create a lexical analyzer that involves parsing the given character stream from the source code of a program, and separating it into discrete token values.
+To create a lexical analyzer that involves parsing the given character stream from an input file. The final product is an output file with all lexemes separated into tokens.
 
-## Programmer:
+## How To:
+- This code was written in C++, and uses a build.sh file in order to run.
+- Type `chmod +x build.sh` in the terminal/command prompt, allowing executable permissions.
+- Type `./build.sh` in the terminal/command prompt to run the program.
+- If you're having difficulties, contact me.
+
+
+## Programmer/Developer:
 Name | Email
 ---- | -----
 Erwin Medina | emedina@csu.fullerton.edu
@@ -12,3 +19,9 @@ Erwin Medina | emedina@csu.fullerton.edu
 Course # | Course Name | Section | Professor
 -------- | ----------- | ------- | ----------
 CS-323 | Compilers & Languages | 03 | Susmitha Padda
+
+## Results / Screenshots
+
+![Input](https://i.imgur.com/Z6zElWg.png)
+---
+![Output](https://i.imgur.com/vElWkKK.png)
--- main.cpp
+++ main.cpp
@@ -1,8 +1,114 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 
+// ********************** //
+// Lexer/Scanner Function //
+// ********************** //
+void Lexer(std::string input_text, std::ofstream &myfile) {
 
+    std::string charHolder; // Variable that will hold character strings
+
+    // ----------------------------------------------------------- //
+    // Creates the vectors for keywords, separators, and operators //
+    // ----------------------------------------------------------- //
+    std::vector<std::string> keywords = {"while", "if", "else", "min", "max", "for"};
+    std::vector<char> separators = {';', '(', ')', '[', ']', '{', '}'};
+    std::vector<char> operators = {'<', '>', '+', '-', '=', '*', '%'};
+
+    // --------------------------- //
+    // Iterates through input_text //
+    // --------------------------- //
+    for (int i = 0; i < input_text.length(); i++) {
+
+        // ------------------------------------------------ //
+        // Handles if white space and nothing in charHolder //
+        // ------------------------------------------------ //
+        if (input_text[i] == ' ' && charHolder == "") {
+            continue;
+        };
+
+        // -------------------- //
+        // Handles the keywords //
+        // -------------------- //
+        if (std::find(keywords.begin(), keywords.end(), charHolder) != keywords.end()) {
+            myfile << "Keyword" << "\t\t\t" << charHolder << std::endl;
+            charHolder = "";
+        }
+
+        // ---------------------- //
+        // Handles the Separators //
+        // ---------------------- //
+        else if (std::find(separators.begin(), separators.end(), input_text[i]) != separators.end()) {
+
+            // Handles if string in charHolder can be converted to float //
+            try {
+                float val = std::stof(charHolder);
+                myfile << "Number" << "\t\t\t" << std::fixed << std::setprecision(2) << val << std::endl;
+                charHolder = "";
+            }
+            // Handles if float failed, and is now an identifier // 
+            catch(const std::invalid_argument) {
+                if (charHolder != "") {
+                    myfile << "Identifier" << "\t\t" << charHolder << std::endl;
+                    charHolder = "";
+                }
+            }
+            myfile << "Separator" << "\t\t" << input_text[i] << std::endl;
+        }
+        
+        // --------------------- //
+        // Handles the Operators //
+        // --------------------- //
+        else if (std::find(operators.begin(), operators.end(), input_text[i]) != operators.end()) {
+            
+            // Handles if string in charHolder can be converted to float //
+            try {
+                float val = std::stof(charHolder);
+                myfile << "Number" << "\t\t\t" << std::fixed << std::setprecision(2) << val << std::endl;
+                charHolder = "";
+            }
+            // Handles if float failed, and is now an identifier //
+            catch(const std::invalid_argument) {
+                if (charHolder != "") {
+                    myfile << "Identifier" << "\t\t" << charHolder  << std::endl;
+                    charHolder = "";
+                }
+            }
+
+            myfile << "Operator" << "\t\t" << input_text[i] << std::endl;
+        }
+
+        // --------------------------------------------------------- //
+        // If none of the above conditions, put char into charHolder //
+        // --------------------------------------------------------- //
+        else {
+            charHolder.push_back(input_text[i]);
+        };
+    }
+};
+
+// ************* //
+// Main Function //
+// ************* //
 int main() {
-    std::cout << ("fuck") << std::endl;
+
+    // ------------------- //
+    // Creates output file //
+    // ------------------- //
+    std::ofstream myfile;
+    myfile.open("output_file.txt");
+    myfile << "Token" << "\t\t\t" << "Lexeme\n" << std::endl;
+
+    // ----------------------------------------- //
+    // Opens input file, gets lines, calls Lexer //
+    // ----------------------------------------- //
+    std::string myText;
+    std::ifstream MyReadFile("input_scode.txt");
+    std::getline(MyReadFile, myText); // Stores line into 'myText'
+    Lexer(myText, myfile);
+
     return 0;
 }
+
